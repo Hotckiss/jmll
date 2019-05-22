85af14ca050101ae8a3fbdc53c7b6586e48b792b
Andrew <kirilenko.haa@gmail.com>
2019-05-22 12:12:23 +0300

    improve speed with presort

--- ml/src/main/java/com/expleague/ml/GridTools.java
+++ ml/src/main/java/com/expleague/ml/GridTools.java
@@ -980,6 +980,146 @@ public class GridTools {
     return new BFGridImpl(rows);
   }
 
+  public static class SortedFeatureWrapper {
+    public double[] sortedFeature;
+
+    public SortedFeatureWrapper(double[] sortedFeature) {
+      this.sortedFeature = sortedFeature;
+    }
+  }
+
+  public static class PermutationWrapper {
+    public int[] permutation;
+
+    public PermutationWrapper(int[] permutation) {
+      this.permutation = permutation;
+    }
+  }
+  /**
+   * Builds naive probability grid
+   * @param ds
+   * @param binFactor
+   * @return
+   */
+  public static BFGrid probabilityGrid_presort(final VecDataSet ds, final int binFactor) {
+    assert (binFactor < ds.length());
+
+    final int dim = ds.xdim();
+    final BFRowImpl[] rows = new BFRowImpl[dim];
+    int bfCount = 0;
+    ArrayList<TIntArrayList> currentBorders = new ArrayList<>();
+
+    ArrayList<SortedFeatureWrapper> sortedFeatures = new ArrayList<>();
+    ArrayList<PermutationWrapper> orders = new ArrayList<>();
+    ArrayList<PermutationWrapper> reverces = new ArrayList<>();
+
+    for (int feature_index = 0; feature_index < dim; feature_index++) {
+      final double[] feature = new double[ds.length()];
+      final ArrayPermutation permutation = new ArrayPermutation(ds.order(feature_index));
+      final int[] order = permutation.direct();
+      final int[] reverse = permutation.reverse();
+      for (int i = 0; i < feature.length; i++)
+        feature[i] = ds.at(order[i]).get(feature_index);
+
+      sortedFeatures.add(new SortedFeatureWrapper(feature));
+      orders.add(new PermutationWrapper(order));
+      reverces.add(new PermutationWrapper(reverse));
+    }
+
+    //initial borders
+    for (int i = 0; i < dim; i++) {
+      TIntArrayList borders = new TIntArrayList();
+      borders.add(ds.length());
+      currentBorders.add(borders);
+    }
+
+    for(int iters = 0; iters < binFactor; iters++) {
+      System.out.println("Iter: " + iters);
+      for (int feature_index = 0; feature_index < dim; feature_index++) {
+        System.out.print("Feature: " + feature_index + " ");
+        final double[] feature = sortedFeatures.get(feature_index).sortedFeature;
+        final int[] order = orders.get(feature_index).permutation;
+        final int[] reverse = reverces.get(feature_index).permutation;
+        boolean haveDiffrentElements = false;
+        for (int i = 1; i < order.length; i++)
+          if (order[i] != order[0])
+            haveDiffrentElements = true;
+        if (!haveDiffrentElements)
+          continue;
+
+        PartitionResult bestFromAll = PartitionResult.makeWorst();
+
+        int bf = 0;
+        for (int paired_feature_index = 0; paired_feature_index < dim; paired_feature_index++) {
+          //System.out.println(paired_feature_index);
+          if (paired_feature_index == feature_index) {
+            continue;
+          }
+
+          final int[] reversePaired = reverces.get(paired_feature_index).permutation;
+
+          int[] binNumberMapper = buildBinsMapper(currentBorders.get(paired_feature_index), reverse, reversePaired);
+
+          PartitionResult bestResult = PartitionResult.makeWorst();
+
+          bestResult = bestPartitionWithMapper_veryFast(binNumberMapper, feature, currentBorders.get(feature_index));
+
+          if (bestFromAll.score < bestResult.score) {
+            bestFromAll = bestResult;
+            bf = paired_feature_index;
+          }
+        }
+
+        //System.out.println(bestFromAll.splitPosition);
+        if (bestFromAll.splitPosition > 1) {
+          System.out.println("BestPaired feature: " + bf);
+          TIntArrayList newBorders = insertBorder(currentBorders.get(feature_index), bestFromAll.splitPosition);
+          currentBorders.set(feature_index, newBorders);
+        } else {
+          System.out.println();
+        }
+
+      }
+    }
+
+    System.out.print("[");
+    for (int i = 0; i < currentBorders.size(); i++) {
+      System.out.print("[");
+      StringBuilder sb = new StringBuilder();
+      for (int j = 0; j < currentBorders.get(i).size() - 1; j++) {
+        sb.append(currentBorders.get(i).get(j) + ", ");
+        //System.out.print(currentBorders.get(i).get(j) + ", ");
+      }
+      if (sb.length() > 0) {
+        sb.delete(sb.length() - 2, sb.length());
+      }
+      System.out.println(sb.toString() + "], ");
+      //System.out.println("]");
+    }
+    System.out.println("]");
+    for (int f = 0; f < dim; f++) {
+      final TIntArrayList borders = currentBorders.get(f);
+      int size = borders.size();
+      final TDoubleArrayList dborders = new TDoubleArrayList();
+      final TIntArrayList sizes = new TIntArrayList();
+      final double[] feature = new double[ds.length()];
+      final ArrayPermutation permutation = new ArrayPermutation(ds.order(f));
+      final int[] order = permutation.direct();
+      for (int i = 0; i < feature.length; i++)
+        feature[i] = ds.at(order[i]).get(f);
+
+      for (int b = 0; b < size - 1; b++) {
+        int borderValue = borders.get(b);
+        dborders.add((feature[borderValue - 1] + feature[borderValue]) / 2.);
+        sizes.add(borderValue);
+      }
+      rows[f] = new BFRowImpl(bfCount, f, dborders.toArray(), sizes.toArray());
+      bfCount += dborders.size();
+    }
+
+    return new BFGridImpl(rows);
+  }
+  
   public static BFGrid probabilityGrid_bigInt(final VecDataSet ds, final int binFactor, boolean useFastAlgorithm) {
     assert (binFactor < ds.length());
 
