ccae87e609348ec95e43caad94dbedc943412198
Andrew <kirilenko.haa@gmail.com>
2019-05-26 18:39:24 +0300

    datasets selection step

---
+++ ml/src/test/java/com/expleague/ml/benchmark/ml/DatasetType.java
@@ -0,0 +1,9 @@
+package com.expleague.ml.benchmark.ml;
+
+public enum DatasetType {
+    FEATURES_TXT,
+    SAME,
+    SAME_LOG,
+    SAME_DUPL_0_25,
+    RANDOM_FUNCS
+}
---
+++ ml/src/test/java/com/expleague/ml/benchmark/ml/DatasetsFactory.java
@@ -0,0 +1,47 @@
+package com.expleague.ml.benchmark.ml;
+
+public class DatasetsFactory {
+    public static String getDatasetName(DatasetType type) {
+        switch (type) {
+            case FEATURES_TXT:
+                return "Features.txt";
+            case SAME:
+                return "Same features";
+            case SAME_LOG:
+                return "Features + log";
+            case SAME_DUPL_0_25:
+                return "Same features + duplicates";
+            case RANDOM_FUNCS:
+                return "Random functions";
+        }
+
+        return "Median division";
+    }
+
+    public static DatasetType getDatasetType(String raw) {
+        switch (raw) {
+            case "Features.txt":
+                return DatasetType.FEATURES_TXT;
+            case "Same features":
+                return DatasetType.SAME;
+            case "Features + log":
+                return DatasetType.SAME_LOG;
+            case "Same features + duplicates":
+                return DatasetType.SAME_DUPL_0_25;
+            case "Random functions":
+                return DatasetType.RANDOM_FUNCS;
+            default:
+                return DatasetType.FEATURES_TXT;
+        }
+    }
+
+    public static String[] getDatasetNames() {
+        String[] res = new String[DatasetType.values().length];
+        int ptr = 0;
+        for (DatasetType type : DatasetType.values()) {
+            res[ptr++] = getDatasetName(type);
+        }
+
+        return res;
+    }
+}
